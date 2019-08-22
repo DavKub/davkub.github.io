5edc082c101e680bb33b1325c5bf0b537da4d453
David Kubos <dav.kubos@gmail.com>
2019-08-22 12:59:42 +0200

    remade to JSON

--- resources/petFamilies.js
+++ resources/petFamilies.js
@@ -1,205 +1,205 @@
-export const petFamilies = [
+[    
     {
-        id: 1,
-        familyName: "Bats",
-        role: "Offense",
-        diet: ["Fruit", "Fungus"],
-        levelRange: "1 - 60",
-        health: "1.00 (Medium)",
-        armor: "1.00 (Low)",
-        damage: "1.07 (High)",
-        skinVariations: 4
+        "id": 1,
+        "familyName": "Bats",
+        "role": "Offense",
+        "diet": ["Fruit", "Fungus"],
+        "levelRange": "1 - 60",
+        "health": "1.00 (Medium)",
+        "armor": "1.00 (Low)",
+        "damage": "1.07 (High)",
+        "skinVariations": 4
     },
 
     {
-        id: 2,
-        familyName: "Bears",
-        role: "Defense",
-        diet: ["Bread", "Cheese", "Fish", "Fruit", "Fungus", "Meat"],
-        levelRange: "5 - 60",
-        health: "1.08 (High)",
-        armor: "1.05 (Medium)",
-        damage: "0.91 (Low)",
-        skinVariations: 7
+        "id": 2,
+        "familyName": "Bears",
+        "role": "Defense",
+        "diet": ["Bread", "Cheese", "Fish", "Fruit", "Fungus", "Meat"],
+        "levelRange": "5 - 60",
+        "health": "1.08 (High)",
+        "armor": "1.05 (Medium)",
+        "damage": "0.91 (Low)",
+        "skinVariations": 7
     },
 
     {
-        id: 3,
-        familyName: "Boars",
-        role: "Defense",
-        diet: ["Bread", "Cheese", "Fish", "Fruit", "Fungus", "Meat"],
-        levelRange: "1 - 60",
-        health: "1.04 (High)",
-        armor: "1.09 (Medium)",
-        damage: "0.90 (Low)",
-        skinVariations: 9
+        "id": 3,
+        "familyName": "Boars",
+        "role": "Defense",
+        "diet": ["Bread", "Cheese", "Fish", "Fruit", "Fungus", "Meat"],
+        "levelRange": "1 - 60",
+        "health": "1.04 (High)",
+        "armor": "1.09 (Medium)",
+        "damage": "0.90 (Low)",
+        "skinVariations": 9
     },
 
     {
-        id: 4,
-        familyName: "Carrion Birds",
-        role: "General",
-        diet: ["Fish", "Meat"],
-        levelRange: "5 - 55",
-        health: "1.00 (Medium)",
-        armor: "1.05 (Medium)",
-        damage: "1.00 (Medium)",
-        skinVariations: 4
+        "id": 4,
+        "familyName": "Carrion Birds",
+        "role": "General",
+        "diet": ["Fish", "Meat"],
+        "levelRange": "5 - 55",
+        "health": "1.00 (Medium)",
+        "armor": "1.05 (Medium)",
+        "damage": "1.00 (Medium)",
+        "skinVariations": 4
     },
 
     {
-        id: 5,
-        familyName: "Cats",
-        role: "Offense",
-        diet: ["Fish", "Meat"],
-        levelRange: "1 - 60",
-        health: "0.98 (Medium)",
-        armor: "Armor: 1.00 (Low)",
-        damage: "1.10 (High)",
-        skinVariations: 17
+        "id": 5,
+        "familyName": "Cats",
+        "role": "Offense",
+        "diet": ["Fish", "Meat"],
+        "levelRange": "1 - 60",
+        "health": "0.98 (Medium)",
+        "armor": "1.00 (Low)",
+        "damage": "1.10 (High)",
+        "skinVariations": 17
     },
 
     {
-        id: 6,
-        familyName: "Crabs",
-        role: "Defense",
-        diet: ["Bread", "Fish", "Fruit", "Fungus"],
-        levelRange: "5 - 44",
-        health: "0.96 (Low)",
-        armor: "1.13 (High)",
-        damage: "0.95 (Low)",
-        skinVariations: 4
+        "id": 6,
+        "familyName": "Crabs",
+        "role": "Defense",
+        "diet": ["Bread", "Fish", "Fruit", "Fungus"],
+        "levelRange": "5 - 44",
+        "health": "0.96 (Low)",
+        "armor": "1.13 (High)",
+        "damage": "0.95 (Low)",
+        "skinVariations": 4
     },
 
     {
-        id: 7,
-        familyName: "Crocolisks",
-        role: "Defense",
-        diet: ["Fish", "Meat"],
-        levelRange: "9 - 60",
-        health: "0.95 (Low)",
-        armor: "1.10 (High)",
-        damage: "1.00 (Medium)",
-        skinVariations: 4
+        "id": 7,
+        "familyName": "Crocolisks",
+        "role": "Defense",
+        "diet": ["Fish", "Meat"],
+        "levelRange": "9 - 60",
+        "health": "0.95 (Low)",
+        "armor": "1.10 (High)",
+        "damage": "1.00 (Medium)",
+        "skinVariations": 4
     },
 
     {
-        id: 8,
-        familyName: "Gorillas",
-        role: "Defense",
-        diet: ["Fruit", "Fungus"],
-        levelRange: "32 - 55",
-        health: "1.04 (High)",
-        armor: "1.00 (Low)",
-        damage: "1.02 (Medium)",
-        skinVariations: 5
+        "id": 8,
+        "familyName": "Gorillas",
+        "role": "Defense",
+        "diet": ["Fruit", "Fungus"],
+        "levelRange": "32 - 55",
+        "health": "1.04 (High)",
+        "armor": "1.00 (Low)",
+        "damage": "1.02 (Medium)",
+        "skinVariations": 5
     },
 
     {
-        id: 9,
-        familyName: "Hyenas",
-        role: "General",
-        diet: ["Fruit", "Meat"],
-        levelRange: "13 - 51",
-        health: "1.00 (Medium)",
-        armor: "1.05 (Medium)",
-        damage: "1.00 (Medium)",
-        skinVariations: 7
+        "id": 9,
+        "familyName": "Hyenas",
+        "role": "General",
+        "diet": ["Fruit", "Meat"],
+        "levelRange": "13 - 51",
+        "health": "1.00 (Medium)",
+        "armor": "1.05 (Medium)",
+        "damage": "1.00 (Medium)",
+        "skinVariations": 7
     },
 
     {
-        id: 10,
-        familyName: "Owls",
-        role: "Offense",
-        diet: ["Meat"],
-        levelRange: "5 - 59",
-        health: "1.00 (Medium)",
-        armor: "Armor: 1.00 (Low)",
-        damage: "1.07 (High)",
-        skinVariations: 2
+        "id": 10,
+        "familyName": "Owls",
+        "role": "Offense",
+        "diet": ["Meat"],
+        "levelRange": "5 - 59",
+        "health": "1.00 (Medium)",
+        "armor": "1.00 (Low)",
+        "damage": "1.07 (High)",
+        "skinVariations": 2
     },
 
     {
-        id: 11,
-        familyName: "Raptors",
-        role: "Offense",
-        diet: ["Meat"],
-        levelRange: "6 - 60",
-        health: "0.95 (Low)",
-        armor: "1.03 (Low)",
-        damage: "1.10 (High)",
-        skinVariations: 7
+        "id": 11,
+        "familyName": "Raptors",
+        "role": "Offense",
+        "diet": ["Meat"],
+        "levelRange": "6 - 60",
+        "health": "0.95 (Low)",
+        "armor": "1.03 (Low)",
+        "damage": "1.10 (High)",
+        "skinVariations": 7
     },
 
     {
-        id: 12,
-        familyName: "Scorpids",
-        role: "Defense",
-        diet: ["Meat"],
-        levelRange: "3 - 59",
-        health: "1.00 (Medium)",
-        armor: "1.10 (High)",
-        damage: "0.94 (Low)",
-        skinVariations: 8
+        "id": 12,
+        "familyName": "Scorpids",
+        "role": "Defense",
+        "diet": ["Meat"],
+        "levelRange": "3 - 59",
+        "health": "1.00 (Medium)",
+        "armor": "1.10 (High)",
+        "damage": "0.94 (Low)",
+        "skinVariations": 8
     },
 
     {
-        id: 13,
-        familyName: "Spiders",
-        role: "Offense",
-        diet: ["Meat"],
-        levelRange: "2 - 60",
-        health: "1.00 (Medium)",
-        armor: "1.00 (Low)",
-        damage: "1.07 (High)",
-        skinVariations: 13
+        "id": 13,
+        "familyName": "Spiders",
+        "role": "Offense",
+        "diet": ["Meat"],
+        "levelRange": "2 - 60",
+        "health": "1.00 (Medium)",
+        "armor": "1.00 (Low)",
+        "damage": "1.07 (High)",
+        "skinVariations": 13
     },
 
     {
-        id: 14,
-        familyName: "Tallstriders",
-        role: "Defense",
-        diet: ["Cheese", "Fruit", "Fungus"],
-        levelRange: "1 - 20",
-        health: "1.05 (High)",
-        armor: "1.00 (Low)",
-        damage: "1.00 (Medium)",
-        skinVariations: 5
+        "id": 14,
+        "familyName": "Tallstriders",
+        "role": "Defense",
+        "diet": ["Cheese", "Fruit", "Fungus"],
+        "levelRange": "1 - 20",
+        "health": "1.05 (High)",
+        "armor": "1.00 (Low)",
+        "damage": "1.00 (Medium)",
+        "skinVariations": 5
     },
 
     {
-        id: 15,
-        familyName: "Turtles",
-        role: "Defense",
-        diet: ["Fish", "Fruit", "Fungus"],
-        levelRange: "15-54",
-        health: "1.00 (Medium)",
-        armor: "1.13 (High)",
-        damage: "0.90 (Low)",
-        skinVariations: 5
+        "id": 15,
+        "familyName": "Turtles",
+        "role": "Defense",
+        "diet": ["Fish", "Fruit", "Fungus"],
+        "levelRange": "15-54",
+        "health": "1.00 (Medium)",
+        "armor": "1.13 (High)",
+        "damage": "0.90 (Low)",
+        "skinVariations": 5
     },
 
     {
-        id: 16,
-        familyName: "Wind Serpents",
-        role: "Offense",
-        diet: ["Bread", "Cheese", "Fish"],
-        levelRange: "15 - 60",
-        health: "1.00 (Medium)",
-        armor: "1.00 (Low)",
-        damage: "1.07 (High)",
-        skinVariations: 6
+        "id": 16,
+        "familyName": "Wind Serpents",
+        "role": "Offense",
+        "diet": ["Bread", "Cheese", "Fish"],
+        "levelRange": "15 - 60",
+        "health": "1.00 (Medium)",
+        "armor": "1.00 (Low)",
+        "damage": "1.07 (High)",
+        "skinVariations": 6
     },
 
     {
-        id: 17,
-        familyName: "Wolves",
-        role: "General",
-        diet: ["Meat"],
-        levelRange: "1 - 61",
-        health: "1.00 (Medium)",
-        armor: "1.05 (Medium)",
-        damage: "1.00 (Medium)",
-        skinVariations: 10
+        "id": 17,
+        "familyName": "Wolves",
+        "role": "General",
+        "diet": ["Meat"],
+        "levelRange": "1 - 61",
+        "health": "1.00 (Medium)",
+        "armor": "1.05 (Medium)",
+        "damage": "1.00 (Medium)",
+        "skinVariations": 10
     }
-];
+]
