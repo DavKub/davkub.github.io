3dfe4f11db9193d43df9c4868e401cf0901dd410
David Kuboš <dav.kubos@sde.cz>
2020-03-24 11:14:29 +0100

    Test build 1

---
+++ precache-manifest.7148e4146a39b419e9af52ba80667f21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c877d8e9bd9db62cedc9ecd9f9e244c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf50718f4067fd54226c",
+    "url": "/static/css/main.4314b0d9.chunk.css"
+  },
+  {
+    "revision": "402e37536a145c8c2c3a",
+    "url": "/static/js/2.2a117bf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2a117bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf50718f4067fd54226c",
+    "url": "/static/js/main.71aa5541.chunk.js"
+  },
+  {
+    "revision": "08c18688b688174884eb",
+    "url": "/static/js/runtime-main.7d96d41e.js"
+  }
+]);
